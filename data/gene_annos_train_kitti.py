43c9f6e6a9d1483a571ff330fc0d02022570cca9
sunpihai <sunpihai@qq.com>
2023-10-07 15:48:46 +0800

    nothing

--- data/gene_annos_train_kitti.py
+++ data/gene_annos_train_kitti.py
@@ -3,28 +3,33 @@ if __name__=='__main__':
     import json
 
     f = open("./data/kitti_eigen_train.txt", "r")
+    data_root = './data/eigen/train'
+    rgb_root = os.path.join(data_root, 'rgb')
+    gt_root = os.path.join(data_root, 'gt')
+
     lines = f.readlines()
+    num = 0
+
+    cam_in = [707.0493, 707.0493, 604.0814, 180.5066]
+    depth_scale = 256.
+
     files = []
     for line in lines:
-        r_path, d_path,  = line.split(' ')
-        if depth_path == "None":
+        splitLine = line.split()
+        r_path, d_path = splitLine[0], splitLine[1]
+        if d_path == "None":
             continue
-        rgb_path = "./eigen/train/rgb/" + r_path
-        depth_path = "./eigen/train/gt/" + d_path
-        cam_in = [707.0493, 707.0493, 604.0814, 180.5066]
-        depth_scale = 256.
-        if os.path.exists(rgb_path) == False:
-            raise RuntimeError(f'File does not exist: {rgb_path}')
-        if os.path.exists(depth_path) == False:
-            raise RuntimeError(f'File does not exist: {depth_path}')
-        
+        rgb_path = os.path.join(rgb_root, r_path)
+        gt_path = os.path.join(gt_path, d_path)
+
         meta_data = {}
         meta_data['cam_in'] = cam_in
         meta_data['rgb'] = rgb_path
-        meta_data['depth'] = depth_path
+        meta_data['depth'] = gt_path
         meta_data['depth_scale'] = depth_scale
         files.append(meta_data)
     files_dict = dict(files=files)
-    with open(osp.join('./data/eigen/train_annotations.json'), 'w') as f:
+
+    with open(os.join(data_root, 'train_annotations.json'), 'w') as f:
         json.dump(files_dict, f)
         
--- gene_eigen_dataset.py
+++ gene_eigen_dataset.py
@@ -5,29 +5,30 @@ if __name__=='__main__':
     dstD = "E:/eigen/train"
     f = open("./data/kitti_eigen_train.txt", "r")
     lines = f.readlines()
+    num = 0
+    num2 = 0
     for line in lines:
-        r_path, d_path,  = line.split(' ')
-        if depth_path == "None":
+        if (num2 > 10):
+            break
+        splitLine = line.split()
+        r_path, d_path = splitLine[0], splitLine[1]
+        if d_path == "None":
             continue
+        
         rgb_path = "E:/kitt-raw/raw/" + r_path
         depth_path = "E:/kitti-depthp/data_depth_annotated/train/" + d_path
+        
         if os.path.exists(depth_path) == False:
             depth_path = depth_path.replace('train', 'val')
+        if os.path.exists(dstD + "/rgb/" + r_path) or os.path.exists(dstD + "/gt/" + d_path):
+            continue
         if os.path.exists(rgb_path) and os.path.exists(depth_path):
+            num = num + 1
+            os.makedirs(os.path.dirname(dstD + "/rgb/" + r_path), exist_ok=True)
             copy(rgb_path, dstD + "/rgb/" + r_path)
+            os.makedirs(os.path.dirname(dstD + "/gt/" + d_path), exist_ok=True)
             copy(depth_path, dstD + "/gt/" + d_path)
-    
-    # dstD = "E:/eigen/test"
-    # f = open("./data/kitti_eigen_test.txt", "r")
-    # lines = f.readlines()
-    # for line in lines:
-    #     r_path, d_path,  = line.split(' ')
-    #     if depth_path == "None":
-    #         continue
-    #     rgb_path = "E:/kitt-raw/raw" + r_path
-    #     depth_path = "E:/kitti-depthp/data_depth_annotated/train" + d_path
-    #     if os.path.exists(depth_path) == False:
-    #         depth_path = depth_path.replace('train', 'val')
-    #     if os.path.exists(rgb_path) and os.path.exists(depth_path):
-    #         copy(rgb_path, dstD + "/rgb/" + r_path)
-    #         copy(depth_path, dstD + "/gt/" + d_path)
+            print(f"NO: {num}, " + rgb_path + " is OK!")
+        elif not(os.path.exists(rgb_path) and os.path.exists(depth_path)):
+            print(rgb_path)
+            num2 = num2 + 1
